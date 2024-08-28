3794240704b118e8de0869ab79690ef1658a3b68
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2024-08-28 20:04:28 +0530

    Finished design and set operator works

--- calculator-react/src/components/Calculator.jsx
+++ calculator-react/src/components/Calculator.jsx
@@ -7,21 +7,38 @@ const Calculator = () =>{
         "AC","DEL","%","/",
         "7","8","9","*",
         "4","5","6","-",
-        "1","2","3","+","-","0","Equals",
+        "1","2","3","+",".","0","Equals",
     ];
 
+    const handleClick = (label) =>{
+        if(label === "AC"){
+            setDisplay("");
+            setShowResult("");
+        }else if (label === "DEL"){
+            setDisplay(display.slice(0, -1));
+        }else if(label === "Equals"){
+            setShowResult("Result");
+        }
+        else{
+            setDisplay(display + label);
+            setShowResult(false);
+        }
+    }
+
     const [showResult, setShowResult] = useState(false);
+    const [display , setDisplay] = useState("");
 
-    const resultClass = ""
-
+    const resultClass = "text-[1.2rem]";
+    const operationClass = "text-[1.2rem] flex-gap-[5px] item-center text-[rgba(255,255,255,0.5)] justify-end";
     return(
         <div className="min-w-[320px] bg-black flex flex-col gap-4 p-4 rounded-2xl">
             <div className="overflow-x-auto bg-[#141414] min-h-[100px] flex items-end justify-end flex-col p-4 rounded-[10px]">
-                <div className={'${showResult ? :}'}>RESULT</div>
+                <div className="text-[25px]">{display}</div>
+                <div className={'${showResult ? resultClass : operationClass} '}>{showResult}</div>
             </div>
-            <div>
+            <div className="grid grid-cols-[repeat(4,1fr)] gap-[0.3 rem]">
                 {keys.map((item, index)=>(
-                    <Keys label={item} key={index}/>
+                    <Keys label={item} key={index} keyClass ={item === 'Equals' && 'equals'} onClick = {() => handleClick(item)}/>
                 ))}
             </div>
         </div>
--- calculator-react/src/components/Keys.jsx
+++ calculator-react/src/components/Keys.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-const Keys = ({label}) =>{
-    return(
-        <div>
+const Keys = ({ label, keyClass, onClick}) => {
+    const equalClass = "col-[span_2] bg-[#4ccdc6] text-[#1a261a] font-semibold hover:bg-[#141414] hover:text-[#fff]";
+    
+    // Determine final class string
+    const finalClass = `bg-[#141414] flex justify-center p-4 rounded-[5px] cursor-pointer items-center m-1 hover:bg-black ${keyClass ? equalClass : ''}`;
+    
+    return (
+        <div className={finalClass} onClick={onClick}>
             {label}
         </div>
-    )
-}
+    );
+};
 
 export default Keys;
